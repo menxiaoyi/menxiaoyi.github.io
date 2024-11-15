487050cffae9adbdc3e2d50f13b52a51b3c290f0
menxiaoyi <140901806+menxiaoyi@users.noreply.github.com>
2024-11-16 01:22:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // script.js
 
 document.getElementById('open-button').addEventListener('click', function() {
-    document.getElementById('nav-container').classList.toggle('active');
+    document.getElementById('nav-container').classList.add('active');
+});
+
+document.getElementById('close-button').addEventListener('click', function() {
+    document.getElementById('nav-container').classList.remove('active');
 });
