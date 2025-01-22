e8cd738f04f8dcd2962ac0e64cb489e98b6bbcad
menxiaoyi <140901806+menxiaoyi@users.noreply.github.com>
2025-01-22 12:42:52 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-
-document.getElementById('open-button').addEventListener('click', function() {
-    document.getElementById('nav-container').classList.add('active');
-});
-
-document.getElementById('close-button').addEventListener('click', function() {
-    document.getElementById('nav-container').classList.remove('active');
-});
